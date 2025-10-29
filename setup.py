31c4dbfe9656f51ac9905ee938db04fad5123e20
rishavk7-bit <ashu.rishav.k77@gmail.com>
2025-10-30 03:09:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='med_chatbot',
+    version='0.1.0',
+    author="Rishav",
+    author_email="ashu.rishav.k77@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
